5b33a18fc2712766a958ab5432d8025b0e9b3e78
xcheko51x <xcheko51x@gmail.com>
2023-01-20 14:12:15 -0600

    Update index.js

--- pruebas_node/index.js
+++ pruebas_node/index.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+})
+
 app.use(bodyParser.json())
 
 const PUERTO = 3000
